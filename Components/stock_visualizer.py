d31705ff7f000cfab412db4492ee1788495d7673
Hayden Fargo <haydenfargo@gmail.com>
2023-05-04 14:29:02 -0700

    fixing stock_visualizer

--- Components/stock_visualizer.py
+++ Components/stock_visualizer.py
@@ -1,42 +1,49 @@
-import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import pandas as pd
-from statsmodels.tsa.arima.model import ARIMA
+import numpy as np
 
-class ARIMAModel:
-    """An Autoregressive Integrated Moving Average (ARIMA) model for time series forecasting.
+class StockVisualizer:
+    """StockVisualizer is a class for visualizing stock price data and predictions.
     """
-    def __init__(self, order=(1, 1, 1)):
-        """Initializes the ARIMAModel with the given ARIMA order.
+    def __init__(self, original_data, true_values, predictions, future_predictions, dates, sequence_length=60):
+        """Initializes the StockVisualizer with given data.
 
         Args:
-            order (tuple, optional): A tuple of three integers representing the (p, d, q) order of the ARIMA model. Defaults to (1, 1, 1).
+            original_data (pd.DataFrame): Original stock price data.
+            true_values (np.array): True stock prices for the test data.
+            predictions (np.array): Predicted stock prices for the test data.
+            future_predictions (np.array): Predicted stock prices for future days.
+            dates (pd.Series): Dates corresponding to the original_data.
+            sequence_length (int, optional): Sequence length used in data preprocessing. Defaults to 60.
         """
-        self.order = order
-        self.model = None
+        self.original_data = original_data
+        self.true_values = true_values
+        self.predictions = predictions
+        self.future_predictions = future_predictions
+        self.dates = dates
+        self.sequence_length = sequence_length
+        self.original_data = self.original_data.iloc[sequence_length:]
+## i can do merge function tojoin pred and og
 
-    def train(self, series):
-        """Trains the ARIMA model using the given time series data.
+## change axis ticks to limit the number of displayed x axis ticks 
 
-        Args:
-            series (array-like): The input time series data for training.
+
+    def visualize_predictions(self):
+        """Visualizes the stock price predictions against the true values.
         """
-        self.model = ARIMA(series, order=self.order)
-        self.model = self.model.fit()
+        plt.figure(figsize=(14, 6))
+        plt.clf() 
 
-    def predict(self, steps):
-        """Predicts future values using the trained ARIMA model.
-
-        Args:
-            steps (int): The number of future time steps to predict.
-
-        Raises:
-            ValueError: Raised if the model has not been trained before making predictions.
-
-        Returns:
-            array-like: The predicted future values.
-        """
-        if self.model is None:
-            raise ValueError("The model must be trained before making predictions")
-
-        forecast, _, _ = self.model.forecast(steps=steps)
-        return forecast
+        plt.locator_params(axis='x', nbins=25)
+        # Plot original data
+        plt.plot(self.dates, self.original_data['Close'], label='Original Data', color='blue')
+        plt.xlabel('Date')
+        plt.ylabel('Stock Price')
+        plt.title('Stock Price Predictions vs True Values')
+        # angle the x-axis labels
+        
+        plt.xticks(rotation=45)
+        # every 5th label is kept'
+        plt.legend()
+        plt.show()
